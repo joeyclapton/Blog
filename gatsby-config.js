4bf6ecbb94868206c6113feb57cb6937d1d2d588
= joeyclapton <= joeyclapton42@gmail.com>
2020-10-26 21:40:11 -0300

    feat: remove starter configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Blog`,
+    description: `A blog about programming, philosophy, life style, wine, beer and other stuffs...
+    `,
+    author: `@joeyclapton`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
