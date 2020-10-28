54516a818dc52ac51ffbf4afad2582ed23e11fd9
= joeyclapton <= joeyclapton42@gmail.com>
2020-10-28 12:40:33 -0300

    refactor: refactor staticQuery component to useStaticQuery hook

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,27 +1,29 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 
-const Profile = () => (
-  <StaticQuery
-    query={graphql`
-      query Metadata {
-        site {
-          siteMetadata {
-            title
-            position
-            description
-          }
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, description, position },
+    },
+  } = useStaticQuery(graphql`
+    query Metadata {
+      site {
+        siteMetadata {
+          title
+          position
+          description
         }
       }
-    `}
-    render={data => (
-      <div>
-        <h1>{data.site.siteMetadata.title}</h1>
-        <h2>{data.site.siteMetadata.position}</h2>
-        <p>{data.site.siteMetadata.description}</p>
-      </div>
-    )}
-  />
-)
+    }
+  `)
+  return (
+    <div>
+      <h1>{data.site.siteMetadata.title}</h1>
+      <h2>{data.site.siteMetadata.position}</h2>
+      <p>{data.site.siteMetadata.description}</p>
+    </div>
+  )
+}
 
 export default Profile
