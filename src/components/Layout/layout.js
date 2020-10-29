4cd14a5041fe57163746b5276d43849d58355b26
= joeyclapton <= joeyclapton42@gmail.com>
2020-10-29 12:56:17 -0300

    feat: add global and reset css

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import Profile from '../Profile/profile'
 import S from './layout.style'
+import GlobalStyles from '../../styles/global'
 
 const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
+      <GlobalStyles />
       <aside>
         <Profile />
       </aside>
