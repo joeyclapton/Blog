270973ad49dc644b0aea5738eb850b9df85da2b7
= joeyclapton <= joeyclapton42@gmail.com>
2020-10-28 23:38:12 -0300

    chore: add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     position: 'Front-end Developer',
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
