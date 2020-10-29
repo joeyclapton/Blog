30373ad7ce3e50951dbe651bf620d2bf7eba2a9a
= joeyclapton <= joeyclapton42@gmail.com>
2020-10-28 23:40:39 -0300

    refactor: create folder for layout component

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Profile from './Profile/profile'
+import Profile from '../Profile/profile'
 
 const Layout = ({ children }) => {
   return (
