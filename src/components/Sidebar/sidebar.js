7a14a8059f2ecf8db546f06d1cba51abf61e25ed
= joeyclapton <= joeyclapton42@gmail.com>
2020-10-29 23:50:33 -0300

    feat: create and style sidebar component

---
+++ src/components/Sidebar/sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Profile from '../Profile/profile'
+
+import S from './sidebar.style'
+
+const Sidebar = () => (
+  <S.SidebarWrapper>
+    <Profile />
+  </S.SidebarWrapper>
+)
+
+export default Sidebar
