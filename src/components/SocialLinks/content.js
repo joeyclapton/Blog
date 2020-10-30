0f4ed96a2c9617b51baf8f7698e62245126534c7
= joeyclapton <= joeyclapton42@gmail.com>
2020-10-30 00:08:07 -0300

    feat: add social links content

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,22 @@
+const username = 'joeyclapton'
+const links = [
+  {
+    label: 'Github',
+    url: `https://github.com/${username}`,
+  },
+  {
+    label: 'Linkedin',
+    url: `https://www.linkedin.com/in/${username}`,
+  },
+  {
+    label: 'Instagram:',
+    url: `https://instagram.com/${username}`,
+  },
+  {
+    label: 'Spotify',
+    url:
+      'https://open.spotify.com/user/pir11bvaly4zw11bjc4zb5plh?si=4uommDNuSmCiicRRvqiXfA',
+  },
+]
+
+export default links
